97067c20ef58ff07f4db6c26bc9c052487a1a5a6
FelipeMtzCh <fmtzcha@gmail.com>
2022-12-03 09:19:00 -0500

    PreEntrega1-Felipe_Martinez

--- js/Areas.js
+++ js/Areas.js
@@ -1,23 +1,30 @@
-let select = parseInt(
-  prompt(
-    "Ingrese el area que desea hallar, 1. cuadrado/2. rectangulo/3. Circulo"
-  )
-);
-const pi = 3.1416;
+do{
+  let select = parseInt(
+    prompt(
+      "Ingrese el area que desea hallar, 1. cuadrado/ 2. rectangulo/ 3. Circulo"
+    )
+  );
+  const pi = 3.1416;
 
-if (select == 1) {
-  let lado = parseInt(prompt("Ingrese el lado en cm"));
-  let resultado = lado * lado;
-  alert("El area del cuadrado es " + resultado + " cm2");
-}
-else if (select == 2) {
-  let ancho = parseInt(prompt("Ingrese el alto en cm"));
-  let alto = parseInt(prompt("Ingrese el ancho en cm"));
-  let resultado = ancho * alto;
-  alert("El area del rectangulo es " + resultado + " cm2");
-}
-else if (select == 3) {
-  let radio = parseInt(prompt("Ingrese el radio en cm"));
-  let resultado = pi * (radio * radio);
-  alert("El area del circulo es " + resultado + " cm2");
-}
+  if (select == 1) {
+    let lado = parseInt(prompt("Ingrese el lado en cm"));
+    let resultado = lado * lado;
+    alert("El area del cuadrado es " + resultado + " cm2");
+  }
+  else if (select == 2) {
+    let ancho = parseInt(prompt("Ingrese el alto en cm"));
+    let alto = parseInt(prompt("Ingrese el ancho en cm"));
+    let resultado = ancho * alto;
+    alert("El area del rectangulo es " + resultado + " cm2");
+  }
+  else if (select == 3) {
+    let radio = parseInt(prompt("Ingrese el radio en cm"));
+    let resultado = 2 * pi * (radio * radio);
+    alert("El area del circulo es " + resultado.toFixed(2) + " cm2");
+  }
+
+  var temp =  parseInt( prompt("Desea calcular otra area? 1.Si / 2. No, salir") );
+
+}while(temp == 1);
+
+var temp = 1;
--- js/CalculadoraIMC.js
+++ js/CalculadoraIMC.js
@@ -1,7 +1,28 @@
-// IMC = peso/ altura 2
+// IMC = peso / altura 2
 
 let peso = parseFloat(prompt("Ingrese su peso en Kg"));
 let altura = parseFloat(prompt("ingrese su altura en m"));
 let IMC = peso / (altura * altura);
 
-alert("Tu IMC es " + IMC.toFixed(3));
+alert("Tu IMC es " + IMC.toFixed(2));
+
+/*
+IMC	                Nivel de peso
+Por debajo de 18.5	Bajo peso
+18.5 – 24.9	        Normal
+25.0 – 29.9	        Sobrepeso
+30.0 o más	        Obesidad
+*/
+
+if (IMC < 18.5){
+    alert('Estas en bajo peso');
+}
+else if (IMC >= 18.5 || IMC <= 24.9){
+    alert('Estas en peso normal');
+}
+else if (IMC = 25.0  || IMC <= 29.9){
+    alert('Estas en sobre peso');
+}
+else if (IMC > 30.0){
+    alert('Tienes obesidad');
+}
